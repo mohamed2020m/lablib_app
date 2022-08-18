cf7f92ea421af3ae10f4a60f2c90c460a785dcac
leeuw <mohamedessabir20@gmail.com>
2022-08-18 04:39:12 +0100

    updating the navBar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -24,97 +24,18 @@ $(function() {
 	siteSticky();
 	// ending of siteSticky
 
-	// starting of siteMenuClone
-	var siteMenuClone = function() {
-
-		$('.js-clone-singUpLoginBtns').each(function() {
-			var $this = $(this);
-			$this.clone().attr('class', 'site-singUpLoginBtns-wrap').appendTo('.site-mobile-menu-body');
-		});
-
-		$('.js-clone-nav').each(function() {
-			var $this = $(this);
-			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-		});
-
-		setTimeout(function() {
-			var counter = 0;
-			$('.site-mobile-menu .has-children').each(function(){
-			var $this = $(this);
-				$this.prepend('<span class="arrow-collapse collapsed">');
-				$this.find('.arrow-collapse').attr({
-					'data-toggle' : 'collapse',
-					'data-target' : '#collapseItem' + counter,
-				});
-				$this.find('> ul').attr({
-					'class' : 'collapse',
-					'id' : 'collapseItem' + counter,
-				});
-				counter++;
-			});
-		}, 1000);
-
-		$('body').on('click', '.arrow-collapse', function(e) {
-			var $this = $(this);
-			if ( $this.closest('li').find('.collapse').hasClass('show') ) {
-				$this.removeClass('active');
-			} else {
-				$this.addClass('active');
-			}
-			e.preventDefault();  
-			
-		});
-
-		$(window).resize(function() {
-			var $this = $(this),
-			w = $this.width();
-			if ( w > 768 ) {
-				if ( $('body').hasClass('offcanvas-menu') ) {
-					$('body').removeClass('offcanvas-menu');
-				}
-			}
-		})
-
-		$('body').on('click', '.js-menu-toggle', function(e) {
-			var $this = $(this);
-			e.preventDefault();
-
-			if ( $('body').hasClass('offcanvas-menu') ) {
-				$('body').removeClass('offcanvas-menu');
-				$this.removeClass('active');
-			} else {
-				$('body').addClass('offcanvas-menu');
-				$this.addClass('active');
-			}
-		}) 
-
-		// click outisde offcanvas
-		$(document).mouseup(function(e) {
-		var container = $(".site-mobile-menu");
-		if (!container.is(e.target) && container.has(e.target).length === 0) {
-			if ( $('body').hasClass('offcanvas-menu') ) {
-				$('body').removeClass('offcanvas-menu');
-			}
-		}
-		});
-	}; 
-
-	siteMenuClone();
-
-	// Ending of siteMenuClone
-
 	// search-box open close js code
 	let navbar = document.querySelector(".navbar");
 	let searchBox = document.querySelector(".search-box .bx-search");
 	// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
 	searchBox.addEventListener("click", ()=>{
-	navbar.classList.toggle("showInput");
-	if(navbar.classList.contains("showInput")){
-		searchBox.classList.replace("bx-search" ,"bx-x");
-	}else {
-		searchBox.classList.replace("bx-x" ,"bx-search");
-	}
+		navbar.classList.toggle("showInput");
+		if(navbar.classList.contains("showInput")){
+			searchBox.classList.replace("bx-search" ,"bx-x");
+		}else {
+			searchBox.classList.replace("bx-x" ,"bx-search");
+		}
 	});
 
 	// sidebar open close js code
@@ -129,7 +50,6 @@ $(function() {
 		menuCloseBtn.onclick = function() {
 		navLinks.style.left = "-100%";
 	}
-
 
 	// sidebar submenu open close js code
 	let categoriesArrow = document.querySelector(".categories-arrow");
